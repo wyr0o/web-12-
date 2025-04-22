48e634e6f6ed0a2faf471fda64209629daf54574
wyr0o <brykin-2006@mail.ru>
2025-04-22 22:32:49 +0300

    Add files via upload

---
+++ вебинар 12 задание 2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <fstream>
+
+int main()
+{
+	std::ifstream fin("C:/files/in.txt");
+	int size = 0;
+	fin >> size;
+	int* arr = new int[size];
+	
+	for (int i = 0; i < size; i++)
+	{
+			fin >> arr[i];
+	}
+		
+	for (int i = size -1; i >=0; i--) 
+	{
+		std::cout << arr[i] << std::endl;
+	}
+	delete[] arr;
+	fin.close();
+}
