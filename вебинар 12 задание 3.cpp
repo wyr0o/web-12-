498b25799d5e0244571537e74b2927b063175d04
wyr0o <brykin-2006@mail.ru>
2025-04-22 22:46:52 +0300

    Add files via upload

---
+++ вебинар 12 задание 3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <fstream>
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	std::ofstream fout("C:/files/out.txt");
+	int size = 0;
+	std::cout << "введите размер массива: ";
+	std::cin >> size;
+	int* arr = new int[size];
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << "arr [" << i << "] = ";
+		std::cin >> arr[i];
+	}
+	fout << size<< std::endl;
+
+	for (int i = size - 1; i >= 0; i--)
+	{
+		fout << arr[i]<< " ";
+	}
+	delete[] arr;
+	fout.close();
+}
