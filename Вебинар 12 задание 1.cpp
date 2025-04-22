35ebe1f1529855c6ff4dd8486b27d257a62c2a4e
wyr0o <brykin-2006@mail.ru>
2025-04-22 22:12:11 +0300

    Add files via upload

---
+++ Вебинар 12 задание 1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <fstream>
+
+int main()
+{
+	SetConsoleOutputCP(65001); //поменял 1251 на 65001, чтобы работал с кодировкой UTF-8. ещё мне помог переход на кодировку ANSI, но решил, что такой вариант универсальнее
+	SetConsoleCP(65001);
+	std::string str;
+	std::ifstream fin("C:/files/in txt.txt");
+	if (fin.is_open()) 
+	{
+		while (!(fin >> str).eof())
+		{
+			std::cout << str << std::endl;
+		}
+	}
+
+}
